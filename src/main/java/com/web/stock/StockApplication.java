d045681fdabe94d4da92e24500a52b0c10a5cdc4
CountyRipper <1097249217@qq.com>
2020-11-02 18:16:24 +0800

    war部署

--- src/main/java/com/web/stock/StockApplication.java
+++ src/main/java/com/web/stock/StockApplication.java
@@ -3,13 +3,20 @@ package com.web.stock;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.web.stock.mapper")
-public class StockApplication {
-
+public class StockApplication extends SpringBootServletInitializer{
+	
 	public static void main(String[] args) {
 		SpringApplication.run(StockApplication.class, args);
+
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(StockApplication.class);
 	}
 
 }

41405eed19e7e92eae7f2e6d7696fed9cdd69bdf
‘Maverick <1097249217@qq.com>
2020-11-12 10:38:06 +0800

    userinfo信息获取完成

--- src/main/java/com/web/stock/Controller/GetController.java
+++ src/main/java/com/web/stock/Controller/GetController.java
@@ -11,16 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller("/getdata")
+@Controller
+@RequestMapping("/getdata")
 public class GetController {
     @Autowired
     private Userservice userservice;// 创建一个userservice
     Logger logger =LoggerFactory.getLogger(GetController.class);
+
     @RequestMapping("/userinfo")
     @ResponseBody
     public User getuserinfo(@RequestParam(value = "username", required = true) String username){
+        logger.info("进入信息获取");
         try {
             User u1 = userservice.getUserByName(username);
+            logger.info("用户信息={}", u1.toString());
             return u1;
         } catch (Exception e) {
             //TODO: handle exception
--- src/main/java/com/web/stock/bean/User.java
+++ src/main/java/com/web/stock/bean/User.java
@@ -20,6 +20,7 @@ public class User {
     Integer id;              //编号
     String username;        //用户名
     String password;
+    String email;
     String sex;             //性别
     Integer age;            //年龄
     String address;         //所在地

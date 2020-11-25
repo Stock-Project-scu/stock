09c475ab3a458e7626597fd78a3eb59efd2c9d6e
‘Maverick <1097249217@qq.com>
2020-11-25 21:30:52 +0800

    更新了数据库

--- src/main/sql/userxm01.sql
+++ src/main/sql/userxm01.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50151
  File Encoding         : 65001
 
- Date: 23/11/2020 22:12:13
+ Date: 25/11/2020 21:30:02
 */
 
 SET NAMES utf8;
@@ -32,13 +32,16 @@ CREATE TABLE `user`  (
   `introduction` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `username`(`username`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
 INSERT INTO `user` VALUES (1, 'ddd', '123456', '1097249217@qq.com', '男', 18, '上海', NULL);
 INSERT INTO `user` VALUES (2, 'dxt', '123456', '1097249217@qq.com', NULL, NULL, NULL, NULL);
-INSERT INTO `user` VALUES (3, 'aaa', '123456', '111@qq.com', '男', 18, '深圳', 'nihao');
+INSERT INTO `user` VALUES (3, 'aaa', '111111', '111@qq.com', '男', 18, '深圳', 'nihao');
+INSERT INTO `user` VALUES (4, 'GHGHGHG', '123456', '1112@qq.com', NULL, NULL, NULL, NULL);
+INSERT INTO `user` VALUES (5, 'GGG', '123456', '43563@11.com', NULL, NULL, NULL, NULL);
+INSERT INTO `user` VALUES (6, 'TTTT', '123456', '436564@56.com', NULL, NULL, NULL, NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;

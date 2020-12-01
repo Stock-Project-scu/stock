b6aa1d25d79859913d2106c42c4f8a295057bb10
CountyRipper <1097249217@qq.com>
2020-12-01 21:23:29 +0800

    修复

--- src/main/java/com/web/stock/service/impl/GetDataServiceImpl.java
+++ src/main/java/com/web/stock/service/impl/GetDataServiceImpl.java
@@ -6,7 +6,6 @@ import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
-
 import com.web.stock.bean.User;
 import com.web.stock.bean.UserProperty;
 import com.web.stock.service.GetDataService;
@@ -19,7 +18,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
-
 
 @Service("GetDataService")
 public class GetDataServiceImpl implements GetDataService {
@@ -48,7 +46,7 @@ public class GetDataServiceImpl implements GetDataService {
             return userpropertyservice.getUserPropertyByName(username);
         } catch (Exception e) {
             logger.info("获取失败");
-            logger.error("错误为={}", e);
+            logger.error("错误为=", e);
             return null;
         }
     }
@@ -74,41 +72,44 @@ public class GetDataServiceImpl implements GetDataService {
             logger.error("获取出错了", e);
             return null;
         }
-        // return null;
     }
 
     @Autowired
     myHttpClient httpclient;
 
     @Override
-    public Map<String,String> getstockcurrentprice(String StockId) {
-        
-        String url = "http://hq.sinajs.cn/list=sh";
-        url=url+StockId;
-        logger.info("stockid={}",url);
-		HttpMethod method = HttpMethod.GET;
-        String data = httpclient.client(url, method);
-		String [] res = data.split("=");
-		String d1 = res[1];
-		String []res2=d1.split("\"");
-        String d2 = res2[1];
-        String []res3 = d2.split(",");
-        
-        Map<String,String> map = new LinkedHashMap<>();
-        logger.info("数组溢出？size res3={}", res3.length);
-        map.put("stockName", res3[0]);
-        map.put("openPrice", res3[1]);
-        map.put("closePrice", res3[2]);
-        map.put("currentPrice", res3[3]);
-        map.put("maxPrice", res3[4]);
-        map.put("minPrice", res3[5]);
-        map.put("buyOnePrice", res3[6]);
-        map.put("sellOnePrice", res3[7]);
-        map.put("dealCount", res3[8]);
-        map.put("dealValue",res3[9]);
-        map.put("buyOneCount", res3[10]);
-        map.put("Integer", res3[11]);
-        return map;
+    public Map<String, String> getstockcurrentprice(String StockId) {
+
+        try {
+            String url = "http://hq.sinajs.cn/list=sh";
+            url = url + StockId;
+            logger.info("stockid={}", url);
+            HttpMethod method = HttpMethod.GET;
+            String data = httpclient.client(url, method);
+            String[] res = data.split("=");
+            String d1 = res[1];
+            String[] res2 = d1.split("\"");
+            String d2 = res2[1];
+            String[] res3 = d2.split(",");
+            Map<String, String> map = new LinkedHashMap<>();
+            logger.info("数组溢出？size res3={}", res3.length);
+            map.put("stockName", res3[0]);
+            map.put("openPrice", res3[1]);
+            map.put("closePrice", res3[2]);
+            map.put("currentPrice", res3[3]);
+            map.put("maxPrice", res3[4]);
+            map.put("minPrice", res3[5]);
+            map.put("buyOnePrice", res3[6]);
+            map.put("sellOnePrice", res3[7]);
+            map.put("dealCount", res3[8]);
+            map.put("dealValue", res3[9]);
+            map.put("buyOneCount", res3[10]);
+            map.put("Integer", res3[11]);
+            return map;
+        } catch (Exception e) {
+            logger.error("错误信息", e);
+            return null;
+        }
     }
 
 }

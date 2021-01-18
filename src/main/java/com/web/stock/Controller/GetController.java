ecf42077f40bb169fcfff4c24cf8a7b0a6bb48b1
CountyRipper <1097249217@qq.com>
2021-01-18 10:47:28 +0800

    更新get

--- src/main/java/com/web/stock/Controller/GetController.java
+++ src/main/java/com/web/stock/Controller/GetController.java
@@ -8,6 +8,7 @@ import com.web.stock.bean.Stock;
 import com.web.stock.bean.StockOrder;
 import com.web.stock.bean.User;
 import com.web.stock.bean.UserProperty;
+import com.web.stock.bean.UserRole;
 import com.web.stock.bean.UserStock;
 import com.web.stock.service.GetDataService;
 
@@ -28,72 +29,77 @@ public class GetController {
     GetDataService getdataservice;
 
     Logger logger = LoggerFactory.getLogger(GetController.class);
-    
-    //获取用户信息
+
+    // 获取用户信息
     @PostMapping("/userinfo")
     @ResponseBody
-    public User getUserInfo(@RequestParam(value = "username", required = true) String username){
+    public User getUserInfo(@RequestParam(value = "username", required = true) String username) {
         return getdataservice.getuserinfo(username);
     }
 
-    //获取用户资产
+    // 获取用户资产
     @PostMapping("/property")
     @ResponseBody
-    public double getUserProperty(@RequestParam(value = "username", required = true) String username){
+    public double getUserProperty(@RequestParam(value = "username", required = true) String username) {
         return getdataservice.getuserproperty(username).getProperty();
     }
 
-    //获取用户持有的股票
+    // 获取用户持有的股票
     @GetMapping("/userstock")
     @ResponseBody
-    public List<UserStock> getUserStock(@RequestParam(value = "username", required = true) String username){
+    public List<UserStock> getUserStock(@RequestParam(value = "username", required = true) String username) {
         return getdataservice.getuserstock(username);
     }
 
-    //获取用户订单记录
+    // 获取用户订单记录
     @GetMapping("/userstockorder")
     @ResponseBody
-    public List<StockOrder> getuserstockorder(@RequestParam(value = "username", required = true) String username){
+    public List<StockOrder> getuserstockorder(@RequestParam(value = "username", required = true) String username) {
         return getdataservice.getuserstockorder(username);
     }
 
-    //获取所有用户信息
+    // 获取所有用户信息
     @PostMapping("/userinfoall")
     @ResponseBody
-    public List<User> getUserInfoAll(){
+    public List<User> getUserInfoAll() {
         return getdataservice.getuserinfoall();
     }
-    //获取所有用户的资产 //无用
+
+    // 获取所有用户的资产 //无用
     @GetMapping("/propertyall")
     @ResponseBody
-    public List<UserProperty> getUserPropertyAll(){
+    public List<UserProperty> getUserPropertyAll() {
         return getdataservice.getuserpropertyall();
     }
-    //获取股票信息
+
+    // 获取股票信息
     @PostMapping("/stockInfo")
     @ResponseBody
-    public Map<String,String> getStockCurrentPrice(@RequestParam(value = "stockid", required = true) String stockid){
+    public Map<String, String> getStockCurrentPrice(@RequestParam(value = "stockid", required = true) String stockid) {
         return getdataservice.getstockcurrentprice(stockid);
     }
+
     @GetMapping("/news")
     @ResponseBody
-    public String getNewsbyPage(@RequestParam(value = "page", required = true )Integer page){
+    public String getNewsbyPage(@RequestParam(value = "page", required = true) Integer page) {
         return getdataservice.getnews(page);
     }
+
     @GetMapping("/marketInfo")
     @ResponseBody
-    public Marketinfo getMarketInfo(@RequestParam(value = "stockid", required = true) String stockid){
+    public Marketinfo getMarketInfo(@RequestParam(value = "stockid", required = true) String stockid) {
         return getdataservice.getmarketinfo(stockid);
     }
-    
+
     @GetMapping("/userstockall")
     @ResponseBody
-    public List<UserStock> getUserStockAll(){
+    public List<UserStock> getUserStockAll() {
         return getdataservice.getuserstockall();
     }
+
     @GetMapping("/userroleall")
     @ResponseBody
-    public List<UserStock> getUserRoleAll(){
+    public List<UserRole> getUserRoleAll() {
         return getdataservice.getuserroleall();
     }
     

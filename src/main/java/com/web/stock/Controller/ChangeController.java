d3282e821d4797a8e16f3825520cf6d7b506b6bb
CountyRipper <1097249217@qq.com>
2021-01-20 13:20:09 +0800

    1/20新修改重置密码和删除用户

--- src/main/java/com/web/stock/Controller/ChangeController.java
+++ src/main/java/com/web/stock/Controller/ChangeController.java
@@ -3,7 +3,14 @@ package com.web.stock.Controller;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.web.stock.bean.UserProperty;
+import com.web.stock.bean.UserRole;
 import com.web.stock.service.ChangeService;
+import com.web.stock.service.StockOrderService;
+import com.web.stock.service.UserPropertyService;
+import com.web.stock.service.UserRoleService;
+import com.web.stock.service.UserStockService;
+import com.web.stock.service.Userservice;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * @author 代相廷 个人信息修改控制器
@@ -21,10 +29,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/change")
 public class ChangeController {
     @Autowired
-    ChangeService changeservice;//引入修改服务
+    ChangeService changeservice;// 引入修改服务
+    @Autowired
+    UserRoleService userroleservice;
     @Autowired
     HttpSession session;
     Logger logger = LoggerFactory.getLogger(ChangeController.class);
+
     // 修改用户信息
     @PostMapping("/userinfo")
     @ResponseBody
@@ -40,24 +51,65 @@ public class ChangeController {
     @PostMapping("/password")
     @ResponseBody
     public Integer changePassord(@RequestParam(value = "repwd", required = true) String password,
-    @RequestParam(value = "oldpwd", required = true) String oldpassword,HttpServletResponse response) {
+            @RequestParam(value = "oldpwd", required = true) String oldpassword, HttpServletResponse response) {
         return changeservice.changePassordService(password, oldpassword, response);
     }
-    //添加资产
+
+    // 添加资产
     @PostMapping("/addMoney")
     @ResponseBody
-    public Integer addMoney(
-    @RequestParam(value = "money", required = true) double money){
-        logger.info("进入修改资产,增加金额={}",money);
+    public Integer addMoney(@RequestParam(value = "money", required = true) double money) {
+        logger.info("进入修改资产,增加金额={}", money);
         String username = session.getAttribute("username").toString();
         return changeservice.addUserPropertyService(username, money);
     }
+
     @PostMapping("/withdrawMoney")
     @ResponseBody
-    public Integer withdrawMoney(
-    @RequestParam(value = "money", required = true) double money){
-        logger.info("进入修改资产,减少金额={}",money);
+    public Integer withdrawMoney(@RequestParam(value = "money", required = true) double money) {
+        logger.info("进入修改资产,减少金额={}", money);
         String username = session.getAttribute("username").toString();
         return changeservice.subUserPropertyService(username, money);
     }
+
+    @PostMapping("/resetPwd")
+    @ResponseBody
+    public Integer resetPwd(@RequestParam(value = "username", required = true) String username,
+            @RequestParam(value = "password", required = true) String password) {
+        String usernamenow = session.getAttribute("username").toString();
+        if (userroleservice.getUserRoleIdbyname(usernamenow) == 0) {
+            logger.info("管理员重置密码");
+            return changeservice.resetUserPassword(username, password);
+        }
+        else{
+            logger.info("权限不够");
+            return 0;
+        }
+    }
+
+    @PostMapping("/deletuser")
+    @ResponseBody
+    public Integer deletUser(@RequestParam(value = "username", required = true) String username,HttpServletResponse response) {
+        logger.info("删除用户={}", username);
+        try {
+            return changeservice.deletuser(username,response);
+        } catch (Exception e) {
+            logger.error("删除失败", e);
+            return 0;
+        }
+    }
+    @PostMapping("/roleid")
+    @ResponseBody
+    public Integer changeroleid(@RequestParam(value = "username", required = true) String username,
+    @RequestParam(value = "roleid", required = true) Integer roleid){
+        try {
+            return changeservice.changeroleid(username, roleid);
+        } catch (Exception e) {
+           return 0;
+        }
+
+    }
+    
+    
+
 }
--- src/main/java/com/web/stock/service/ChangeService.java
+++ src/main/java/com/web/stock/service/ChangeService.java
@@ -7,4 +7,7 @@ public interface ChangeService {
     public Integer changePassordService(String password,String oldpassword,HttpServletResponse response);//修改密码 
     public Integer addUserPropertyService(String username,double property);//添加用户资产
     public Integer subUserPropertyService(String username,double property);//减少用户资产
+    public Integer resetUserPassword(String username,String password);//重置密码
+    public Integer deletuser(String username,HttpServletResponse response);//根据用户名删除用户
+    public Integer changeroleid(String username,Integer roleid);//修改权限
 }
